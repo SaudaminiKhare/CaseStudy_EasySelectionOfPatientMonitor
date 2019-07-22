e189dcb27a3f783f581a8942dbbf0afc7c2a9eb1
Varsha jain <53017214+v261196@users.noreply.github.com>
2019-07-22 15:27:48 +0530

    Add files via upload

---
+++ sampleconapp.cpp
@@ -0,0 +1,30 @@
+
+#include<iostream>
+using namespace std;
+
+//struct CA
+class CA
+{
+private:
+	int a, b;
+public:
+	void input()   // void input(CA* const this)
+	{
+		cout << "enter 2 nos" << endl;
+		cin >> this->a >> this->b;
+	}
+	void print()   // void print(CA* const this)
+	{
+		cout << "a=" << this->a << ",b=" << this->b << endl;
+	}
+};
+//--- class consumer code----
+int main()
+{
+	cout << "size of CA object" << sizeof(CA) << endl;
+	CA obj1;
+	obj1.input();   // CA::input(&obj1);
+	obj1.print();   // CA::print(&obj1);
+	system("pause");
+	return 0;
+}
